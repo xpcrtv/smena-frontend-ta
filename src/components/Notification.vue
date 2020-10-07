1c9911bf30a989b436bdef7334eb370aabff3ce6
Ruslan <20889274+xpcrtv@users.noreply.github.com>
2020-10-07 22:32:31 +0500

    add: home and notification components

---
+++ src/components/Notification.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="app-notification">
+    <v-slide-y-transition>
+      <v-alert
+        v-if="error"
+        border="left"
+        colored-border
+        type="error"
+        elevation="4"
+        width="400px"
+      >
+        {{ error }}
+      </v-alert>
+    </v-slide-y-transition>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    error() {
+      return this.$store.getters.errorMessage;
+    }
+  },
+  updated() {
+    const { commit } = this.$store;
+    setTimeout(() => commit('clearError'), 3000);
+  }
+};
+</script>
+
+<style>
+.app-notification {
+  position: fixed;
+  top: 90px;
+  right: 20px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-row align="center" justify="center">
+    <v-col cols="12" sm="8" md="4"
+      ><v-card class="mx-auto" max-width="400" v-if="user">
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="user.avatar"
+        />
+        <v-card-title>{{ user.username }}</v-card-title>
+        <v-card-text class="text--primary">
+          <div>{{ user.about }}</div>
+        </v-card-text>
+      </v-card>
+    </v-col></v-row
+  >
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  computed: {
+    user() {
+      return this.$store.state.user.user;
+    }
+  },
+  created() {
+    this.$store.dispatch('getUserInfo');
   }
 };
 </script>
