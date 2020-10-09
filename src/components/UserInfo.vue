fb998f773302e9a9e3b1dcfe4eab8f08ac057af1
xpcrtv <20889274+xpcrtv@users.noreply.github.com>
2020-10-09 21:34:17 +0500

    fix: move user profile to own component

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card class="mx-auto" max-width="400" v-if="user">
+    <v-img class="white--text align-end" height="200px" :src="user.avatar" />
+    <v-card-title>{{ user.username }}</v-card-title>
+    <v-card-text class="text--primary">
+      <div>{{ user.about }}</div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  computed: {
+    user() {
+      return this.$store.state.user.user;
+    }
+  },
+  created() {
+    this.$store.dispatch('getUserInfo');
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,17 @@
 <template>
   <v-row align="center" justify="center">
-    <v-col cols="12" sm="8" md="4"
-      ><v-card class="mx-auto" max-width="400" v-if="user">
-        <v-img
-          class="white--text align-end"
-          height="200px"
-          :src="user.avatar"
-        />
-        <v-card-title>{{ user.username }}</v-card-title>
-        <v-card-text class="text--primary">
-          <div>{{ user.about }}</div>
-        </v-card-text>
-      </v-card>
-    </v-col></v-row
-  >
+    <v-col cols="12" sm="8" md="4">
+      <user-info />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
+import UserInfo from '@/components/UserInfo.vue';
 export default {
   name: 'Home',
-  computed: {
-    user() {
-      return this.$store.state.user.user;
-    }
-  },
-  created() {
-    this.$store.dispatch('getUserInfo');
+  components: {
+    UserInfo
   }
 };
 </script>
